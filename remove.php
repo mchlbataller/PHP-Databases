d477c7e2e780174f3ed11f1bbd6a708ba6fdd2d2
mchlbataller <mchlbataller@gmail.com>
2021-03-09 01:02:37 +0800

    Prettified the code

--- remove.php
+++ remove.php
@@ -1,14 +1,13 @@
 <?php
 require_once("services/database.php");
 
-if ( AuthenticationService::delete($_GET['email'])) {
+if (AuthenticationService::delete($_GET['email'])) {
     echo "Success! Going back to homepage...";
     echo <<<END
         <script>
             setTimeout(() => document.location.replace('/'), 3000);
         </script>
     END;
-}
-else {
+} else {
     echo "Failed. Please contact the administrator for more details.";
 }
